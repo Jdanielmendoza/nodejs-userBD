322c86182f7ef35dde303786b7a49c6144db3b62
Jose Daniel Mendoza <jdanielmendoza987@gmail.com>
2022-10-12 23:33:26 -0400

    modify err

--- src/config/dbconnection.js
+++ src/config/dbconnection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql'); 
  module.exports = () => {
-    const DB_HOST = process.env. DB_HOST || 'localhost';
+    const DB_HOST = process.env.DB_HOST || 'localhost';
     const DB_USER = process.env.DB_USER || 'root';
     const DB_PASSWORD= process.env.DB_PASSWORD || '123456789';
     const DB_NAME = process.env.DB_NAME || 'usuarios';
